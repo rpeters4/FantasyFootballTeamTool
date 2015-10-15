e04b86aa975dacbf8105b712137958906766a2d3
BuffaloGill <mjgill2@buffalo.edu>
2015-10-15 00:57:20 -0400

    Fixed everything but dups, damnit dups

--- FantasyFootballTool.py
+++ FantasyFootballTool.py
@@ -36,11 +36,11 @@ def addRoster(leagueName, teamName):
                 print 'ERROR: League does not exist\n'
                 answer = input('Would you like to add a league? (y/n) ')
                 while True is True:
-                    if answer is in ['y', 'Y', 'Yes', 'yes']:
+                    if answer in ['y', 'Y', 'Yes', 'yes']:
                         addLeague(leagueName)
                         addRoster(leagueName, teamName)
                         break
-                    elif answer is in['n', 'N', 'No', 'no']:
+                    elif answer in ['n', 'N', 'No', 'no']:
                         break
                     else:
                         answer = input('Invalid response please try again (y/n)')
@@ -48,56 +48,66 @@ def addRoster(leagueName, teamName):
         print 'ERROR: League does not exist\n'
         answer = input('Would you like to add a league? (y/n) ')
         while True is True:
-            if answer is in ['y', 'Y', 'Yes', 'yes']:
+            if answer in ['y', 'Y', 'Yes', 'yes']:
                 addLeague(leagueName)
                 addRoster(leagueName, teamName)
                 break
-            elif answer is in['n', 'N', 'No', 'no']:
+            elif answer in ['n', 'N', 'No', 'no']:
                 break
             else:
                 answer = input('Invalid response please try again (y/n)')
             
 def addPlayer(leagueName, rosterName, playerName, playerTeam):
-    foundLeague = False
-    foundRoster = False
-    for l in leagueLists:
-        if l.leagueName is leagueName and foundLeague is False:
-            foundLeague = True
-            for r in l.rosters:
-                if r.teamName is rosterName and foundRoster is False:
-                    foundRoster = True
-                    teamName = nflgame.standard_team(playerTeam)
-                    playerFound = nflgame.find(playerName, team=teamName)
-                    if playerFound:
-                        p = playerFound[0]
-                        playerToAdd = player(p.player_id, p.first_name, p.last_name, p.team, p.position)
-                        r.players.append(playerToAdd)
+    if leagueLists:
+        for l in leagueLists:
+            if l.leagueName is leagueName:
+                for r in l.rosters:
+                    if r.teamName is rosterName:
+                        teamName = nflgame.standard_team(playerTeam)
+                        playerFound = nflgame.find(playerName, team=teamName)
+                        if playerFound:
+                            p = playerFound[0]
+                            playerToAdd = player(p.player_id, p.first_name, p.last_name, p.team, p.position)
+                            r.players.append(playerToAdd)
+                            break
+                        else:   
+                            print 'ERROR: Player does not exist\n'
+                    elif r is l.rosters[-1]:
+                        print 'ERROR: Roster does not exist\n'
+                        answer = input('Would you like to add a team? (y/n) ')
+                        while True is True:
+                            if answer in ['y', 'Y', 'Yes', 'yes']:
+                                addRoster(leagueName, rosterName)
+                                addPlayer(leagueName, rosterName, playerName, playerTeam)
+                                break
+                            elif answer in ['n', 'N', 'No', 'no']:
+                                break
+                            else:
+                                answer = input('Invalid response please try again (y/n)')
+                    break                        
+            elif l is leagueLists[-1]:
+                print 'ERROR: League does not exist\n'
+                answer = input('Would you like to add a league? (y/n) ')
+                while True is True:
+                    if answer in ['y', 'Y', 'Yes', 'yes']:
+                        addLeague(leagueName)
+                        addRoster(leagueName, rosterName)
+                        addPlayer(leagueName, rosterName, playerName, playerTeam)
+                        break
+                    elif answer in ['n', 'N', 'No', 'no']:
+                        break
                     else:
-                        'ERROR: Player %s not found\n' % playerName
-                elif r.teamName is not rosterName and foundRoster is False:
-                    print 'ERROR: Team does not exist\n'
-                    print 'Would you like to add a team? '
-                    answer = input('(y/n)')
-                    while True:
-                        if answer is 'y' or 'Y' or 'Yes' or 'yes':
-                            addRoster(leagueName, RosterName)
-                            break
-                        elif answer is not 'n' or 'N' or 'No' or 'no':
-                            print 'Invalid response please try again'
-                            answer = input('(y/n)')
-                        else:
-                            break
-                        
-        elif l.leagueName is not leagueName and foundLeague is False:
-            print 'ERROR: League does not exist\n'
-            print 'Would you like to add a league? '
-            answer = input('(y/n)')
-            while True:
-                if answer is 'y' or 'Y' or 'Yes' or 'yes':
-                    addLeague(leagueName)
-                    break
-                elif answer is not 'n' or 'N' or 'No' or 'no':
-                    print 'Invalid response please try again'
-                    answer = input('(y/n)')
-                else:
-                    break
+                        answer = input('Invalid response please try again (y/n)')
+    else:
+        print 'ERROR: League does not exist\n'
+        answer = input('Would you like to add a league? (y/n) ')
+        while True is True:
+            if answer in ['y', 'Y', 'Yes', 'yes']:
+                addLeague(leagueName)
+                addRoster(leagueName, rosterName)
+                addPlayer(leagueName, rosterName, playerName, playerTeam)
+                break
+            elif answer in ['n', 'N', 'No', 'no']:
+                break
+            else:
+                answer = input('Invalid response please try again (y/n)')
