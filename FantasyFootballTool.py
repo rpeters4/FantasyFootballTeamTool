0e6c45d2bf9ddf5e864722e715caf6b00e496302
BuffaloGill <mjgill2@buffalo.edu>
2015-10-14 22:56:38 -0400

    Nearly finished framework class

--- FantasyFootballTool.py
+++ FantasyFootballTool.py
@@ -34,14 +34,33 @@ def addRoster(leagueName, teamName):
             print "League does not exist"
             #want to add league?  
             
-def addPlayer(leagueName,rosterName, playerName):
+def addPlayer(leagueName, rosterName, playerName, playerTeam):
+    leagueFound = False
+    teamFound = False
     for l in leagueLists:
         if l.leagueName is leagueName:
+            leagueFound = True
             for r in l.rosters:
                 if r.teamName is rosterName:
-                    plIDs=find(playerName,team=None)
-					if not plIDs
-						print "couldn't find player"
-					else
-						player.player_id = plID[0]
-						
+                    teamFound
+                    teamName = nflgame.standard_team(playerTeam)
+                    playerFound = nflgame.find(playerName, team=teamName)
+                    if playerFound:
+                        p = playerFound[0]
+                        playerToAdd = player(p.player_id, p.first_name, p.last_name, p.team, p.position)
+                        r.players.append(playerToAdd)
+                    else:
+                        'ERROR: Player %s not found\n' % playerName
+    if not leagueFound:
+        print 'ERROR: League %s not found\n' % leagueName
+    if not teamFound:
+        print 'ERROR: Team %s not found\n' % rosterName
+
+          
+addLeague('t')
+addRoster('t', 'test')
+addPlayer('t', 'test', 'tom brady', 'NE')
+addPlayer('NOTREAL','test','tom brady', 'NE')
+addPlayer('t','tiffingtonbanderbotter','tom brady','NE')
+addPlayer('t','test','Fakel McFakington','NE')
+
