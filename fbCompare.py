0e694992a38f02fea13c5bb9a9f09e0411ba35e9
BuffaloGill <mjgill2@buffalo.edu>
2015-10-16 19:09:34 -0400

    Comparison is done, except for defenses

--- fbCompare.py
+++ fbCompare.py
@@ -8,8 +8,14 @@ def playerCompare():
     year1 = int(raw_input("Enter year: \n"))
     w1 = raw_input("Enter week(s): \n")
     weeks1 = map(int, w1.split())
-    points1 = fptest.fantasypoints(player1, year1, weeks1)
     weeksPlayed1 = 0
+    player2 = raw_input("Enter player: \n")
+    year2 = int(raw_input("Enter year: \n"))
+    w2 = raw_input("Enter week(s): \n")
+    weeks2 = map(int, w2.split())
+    points1 = fptest.fantasypoints(player1, year1, weeks1)
+    points2 = fptest.fantasypoints(player2, year2, weeks2)
+    weeksPlayed2 = 0
     for i in weeks1:
         if str(i).isdigit():
             weeksPlayed1 = weeksPlayed1 + 1
@@ -19,14 +25,6 @@ def playerCompare():
             if w[-1] < curWeek and nflgame.find(player1, team=None)[0].team == t:
                 weeksPlayed1 = weeksPlayed1 - 1
                 break
-    average1 = points1 / weeksPlayed1
-    print average1
-    player2 = raw_input("Enter player: \n")
-    year2 = int(raw_input("Enter year: \n"))
-    w2 = raw_input("Enter week(s): \n")
-    weeks2 = map(int, w2.split())
-    points2 = fptest.fantasypoints(player2, year2, weeks2)
-    weeksPlayed2 = 0
     for i in weeks2:
         if str(i).isdigit():
             weeksPlayed2 = weeksPlayed2 + 1
@@ -36,6 +34,8 @@ def playerCompare():
             if w[-1] < curWeek and nflgame.find(player2, team=None)[0].team == t:
                 weeksPlayed2 = weeksPlayed2 - 1
                 break
+    average1 = points1 / weeksPlayed1
     average2 = points2 / weeksPlayed2
-    print average2
+    print '%s\'s average is %.2f' % (player1, average1)
+    print '%s\'s average is %.2f' % (player2, average2)
     
