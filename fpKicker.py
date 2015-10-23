fc62b1d7d4178f0086ed0814caba67917cc91c11
BuffaloGill <mjgill2@buffalo.edu>
2015-10-23 14:25:07 -0400

    fixed fpkicker for real

--- fpKicker.py
+++ fpKicker.py
@@ -1,14 +1,13 @@
 import nflgame
 #calculates stats of kicker but does not add bonus for kickers at distances 40+ or 50+
-def kickerScore(playerName, year, week):
+def kickerScore(player, year, week):
     lf = player
     statistics = lf.stats(year, week)
-    #print statistics.stats
     if 'kicking_xpmissed' in statistics.stats:
         xpMissed = statistics.stats['kicking_xpmissed']
     #print statistics.stats['kicking_xpmissed']
     if 'kicking_xpmade' in statistics.stats:
-    xpMade = statistics.stats['kicking_xpmade']
+        xpMade = statistics.stats['kicking_xpmade']
     #print statistics.stats['kicking_xpmade']
     if 'kicking_fgm' in statistics.stats:
         fgMade = statistics.stats['kicking_fgm']
@@ -17,4 +16,4 @@ def kickerScore(playerName, year, week):
         fgMissed = statistics.stats['kicking_fga'] - statistics.stats['kicking_fgm']
     #print statistics.stats['kicking_fga'] - statistics.stats['kicking_fgm']
     points = xpMade*1 - xpMissed*1 + fgMade*3 - fgMissed*3
-    print points
+    return points
