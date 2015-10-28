183313bcbbf8d7cba06829de6f8b50d2b746a145
BuffaloGill <mjgill2@buffalo.edu>
2015-10-28 16:49:06 -0400

    Defense is working

--- fpDefense.py
+++ fpDefense.py
@@ -1,7 +1,6 @@
 import nflgame
 
-def defensePoints(s, w, t):
-    games = nflgame.games_gen(s, w, t, t)
+def defensePlays(team, year, week, games):
     plays = nflgame.combine_plays(games)
     dfrectd=0
     dffum=0
@@ -11,10 +10,9 @@ def defensePoints(s, w, t):
     punttd=0
     kicktd=0
     dsafe=0
-    for p in plays.filter(team__ne=t):
-        if p.defense_sk > 0:  #checked
-            dsk += 1
-        if p.defense_ffum > 0: #checked
+    sack=0
+    for p in plays.filter(team__ne=team):
+        if p.defense_ffum > 0: 
             dffum += 1
         if p.defense_int > 0:
             dint += 1
@@ -28,72 +26,99 @@ def defensePoints(s, w, t):
             punttd += 1
         if p.kickret_tds > 0:
             kicktd += 1
-    #endfor
+        if p.defense_sk > 0:
+            sack += 1
+    return 2*dffum + 2*dint + 6*dinttd + 6*dfrectd + 2*dsafe + 6*punttd + 6*kicktd + 1*sack
 
-    return dsk + 2*dffum + 2*dint + 6*dinttd + 6*dfrectd + 2*dsafe + 6*punttd + 6*kicktd
-
-def defensePA(s,w,t):
-# We want the games Buffalo played in 2013
-# Get the games Buffalo played in the 2013 Regular Season
-    games = nflgame.games_gen(s, w , home=t, away=t, kind= 'REG')
-    pa = 0.0;
-
-    # Iterate through the games
-    for g in games:
-        # If Buffalo was home, add the score_home to the points for list and the score_away to the points against list
-        if g.home == t:
-            print g.score_home
-            print g.stats_home[1]
-            print g.score_away
-            if g.score_away == 0.0:
-                pa = 5.0
-            if g.score_away > 0.0 and g.score_away <=6.0:
-                pa = 4.0
-            if g.score_away > 6.0 and g.score_away <=13.0:
-                pa = 3.0
-            if g.score_away > 13.0 and g.score_away <=17.0:
-                pa = 1.0
-            if g.score_away > 17.0 and g.score_away <=27.0:
-                pa = 0.0
-            if g.score_away > 27.0 and g.score_away <=34.0:
-                pa = -1.0
-            if g.score_away > 34.0 and g.score_away <=45.0:
-                pa = -3.0
-            if g.score_away > 45.0:
-                pa = -5.0
-            # If Buffalo was away, add the score_away to the points for list and the score_home to the points against list
+def defensePA(team, year, week, games):
+    games = nflgame.games_gen(year, week, home=team, away=team)
+    pa = 0
+    for g in games:  
+        if g.home == team:
+            if g.score_away == 0:
+                pa = 5
+            if g.score_away > 0 and g.score_away <=6:
+                pa = 4
+            if g.score_away > 6 and g.score_away <=13:
+                pa = 3
+            if g.score_away > 13 and g.score_away <=17:
+                pa = 1
+            if g.score_away > 17 and g.score_away <=27:
+                pa = 0
+            if g.score_away > 27 and g.score_away <=34:
+                pa = -1
+            if g.score_away > 34 and g.score_away <=45:
+                pa = -3
+            if g.score_away > 45:
+                pa = -5
         else:
-            print g.score_away
-            print g.stats_away[1]
-            print g.score_home
-            if g.score_home == 0.0:
-                pa = 5.0
-            if g.score_home > 0.0 and g.score_home <=6.0:
-                pa = 4.0
-            if g.score_home > 6.0 and g.score_home <=13.0:
-                pa = 3.0
-            if g.score_home > 13.0 and g.score_home <=17.0:
-                pa = 1.0
-            if g.score_home > 17.0 and g.score_home <=27.0:
-                pa = 0.0
-            if g.score_home > 27.0 and g.score_home <=34.0:
-                pa = -1.0
-            if g.score_home > 34.0 and g.score_home <=45.0:
-                pa = -3.0
+            if g.score_home == 0:
+                pa = 5
+            if g.score_home > 0 and g.score_home <=6:
+                pa = 4
+            if g.score_home > 6 and g.score_home <=13:
+                pa = 3
+            if g.score_home > 13 and g.score_home <=17:
+                pa = 1
+            if g.score_home > 17 and g.score_home <=27:
+                pa = 0
+            if g.score_home > 27 and g.score_home <=34:
+                pa = -1
+            if g.score_home > 34 and g.score_home <=45:
+                pa = -3
             if g.score_home >45.0:
-                pa = -5.0
-            #endif
-        return pa
-        #endif
-    #endfor  
-#endDef
+                pa = -5
+    return pa
 
+def defenseYA(team, year, week, games):
+    games = nflgame.games_gen(year, week, home=team, away=team)
+    ya = 0
+    for g in games:
+        if g.home == team:
+            if g.stats_away[1] < 100:
+                ya = 5
+            if g.stats_away[1] >= 100 and g.stats_away[1] <= 199:
+                ya = 3
+            if g.stats_away[1] >= 200 and g.stats_away[1] <= 299:
+                ya = 2
+            if g.stats_away[1] >= 300 and g.stats_away[1] <= 349:
+                ya = 0
+            if g.stats_away[1] >= 350 and g.stats_away[1] <= 399:
+                ya = -1
+            if g.stats_away[1] >= 400 and g.stats_away[1] <= 449:
+                ya = -3
+            if g.stats_away[1] >= 450 and g.stats_away[1] <= 499:
+                ya = -5
+            if g.stats_away[1] >= 500 and g.stats_away[1] <= 549:
+                ya = -6
+            if g.stats_away[1] >= 550:
+                ya = -7
+        else:
+            if g.stats_home[1] < 100:
+                ya = 5
+            if g.stats_home[1] >= 100 and g.stats_home[1] <= 199:
+                ya = 3
+            if g.stats_home[1] >= 200 and g.stats_home[1] <= 299:
+                ya = 2
+            if g.stats_home[1] >= 300 and g.stats_home[1] <= 349:
+                ya = 0
+            if g.stats_home[1] >= 350 and g.stats_home[1] <= 399:
+                ya = -1
+            if g.stats_home[1] >= 400 and g.stats_home[1] <= 449:
+                ya = -3
+            if g.stats_home[1] >= 450 and g.stats_home[1] <= 499:
+                ya = -5
+            if g.stats_home[1] >= 500 and g.stats_home[1] <= 549:
+                ya = -6
+            if g.stats_home[1] >= 550:
+                ya = -7
+    return ya
 
-#TESTING XDXD
-print defensePoints(2015, None, "NE")             #Get all sacks earned by Baltimore defense in all of 2015
-print defensePoints(2015, 1, "NE")                #Calc all the fantasy points for week 1
-print defensePoints(2015, 2, "NE")                #Calc all the fantasy points for week 2
-print defensePoints(2015, [1,2], "NE")            #calc total points for week 1 and week 2
-
-
-print defensePA(2015,7,'NE')
+def fpDefense(team, year, week):
+    games = nflgame.games_gen(year, week, team, team)
+    points = 0
+    points = points + defensePlays(team, year, week, games)
+    points = points + defensePA(team, year, week, games)
+    points = points + defenseYA(team, year, week, games)
+    return points
+    
