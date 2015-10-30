5059f8862b51ab15af4403a075f28f267fad24fd
BuffaloGill <mjgill2@buffalo.edu>
2015-10-30 15:47:21 -0400

    Defense fixed and just need byes for defenses

--- fbPlayerPoints.py
+++ fbPlayerPoints.py
@@ -12,17 +12,26 @@ def playerPoints(playerInfo):
             points = fpKicker.kickerScore(playerFound[0], playerInfo[1], playerInfo[2])
         else:
             points = fpPlayer.fantasyPoints(playerFound[0], playerInfo[1], playerInfo[2])
+        for i in playerInfo[2]:
+            if str(i).isdigit():
+                weeksPlayed = weeksPlayed + 1
+        for w in WeekLists.byeWeeks:
+            for t in w:
+                curWeek = nflgame.live.current_year_and_week()[1]
+                if w[-1] < curWeek and nflgame.find(playerInfo[0], team=None)[0].team == t and w[-1] in playerInfo[2]:
+                    weeksPlayed1 = weeksPlayed1 - 1
+                    break
+        average = points / weeksPlayed
+        return average
+    elif playerInfo[0] != None:
+        team = nflgame.standard_team(playerInfo[0])
+        if team == None:
+            return float('inf')
+        playerInfo[0] = team
+        points = fpDefense.fpDefense(playerInfo[0], playerInfo[1], playerInfo[2])
+        weeksPlayed = nflgame.live.current_year_and_week()[1]
+        #factor in bye week
+        return points
     else:
         return float('inf')
-    for i in playerInfo[2]:
-        if str(i).isdigit():
-            weeksPlayed = weeksPlayed + 1
-    for w in byeWeekLists.byeWeeks:
-        for t in w:
-            curWeek = nflgame.live.current_year_and_week()[1]
-            if w[-1] < curWeek and nflgame.find(playerInfo[0], team=None)[0].team == t and w[-1] in playerInfo[2]:
-                weeksPlayed1 = weeksPlayed1 - 1
-                break
-    average = points / weeksPlayed
-    return average
     
