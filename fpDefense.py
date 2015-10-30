7fb71e4d584398df3ac0543d328626a9dcaea560
rtr3 <rtr3@buffalo.edu>
2015-10-30 14:57:33 -0400

    I shouldn't be doing this

--- fpDefense.py
+++ fpDefense.py
@@ -26,66 +26,6 @@ def defensePlays(team, year, week, games):
             punttd += 1
         if p.kickret_tds > 0:
             kicktd += 1
-<<<<<<< HEAD
-        #endif
-    #endfor
-    return dsk + 2*dffum + 2*dint + 6*dinttd + 6*dfrectd + 2*dsafe + 6*punttd + 6*kicktd
-#enddef
-
-def defensePA(s,w,t):
-    games = nflgame.games_gen(s, w , home=t, away=t, kind= 'REG')
-    pa = 0.0;
-
-    # Iterate through the games
-    for g in games:
-        # If Buffalo was home, add the score_home to the points for list and the score_away to the points against list
-        if g.home == t:
-            print g.score_home
-            print g.stats_home[1] #this will print the total yards for home team
-            print g.score_away
-            if g.score_away == 0.0:
-                pa = 5.0
-            if g.score_away > 0.0 and g.score_away <=6.0:
-                pa = 4.0
-            if g.score_away > 6.0 and g.score_away <=13.0:
-                pa = 3.0
-            if g.score_away > 13.0 and g.score_away <=17.0:
-                pa = 1.0
-            if g.score_away > 17.0 and g.score_away <=27.0:
-                pa = 0.0
-            if g.score_away > 27.0 and g.score_away <=34.0:
-                pa = -1.0
-            if g.score_away > 34.0 and g.score_away <=45.0:
-                pa = -3.0
-            if g.score_away > 45.0:
-                pa = -5.0
-            # If Buffalo was away, add the score_away to the points for list and the score_home to the points against list
-        else:
-            print g.score_away
-            print g.stats_away[1] #this will print the total yards for the away team, from TeamStats tuple
-            print g.score_home
-            if g.score_home == 0.0:
-                pa = 5.0
-            if g.score_home > 0.0 and g.score_home <=6.0:
-                pa = 4.0
-            if g.score_home > 6.0 and g.score_home <=13.0:
-                pa = 3.0
-            if g.score_home > 13.0 and g.score_home <=17.0:
-                pa = 1.0
-            if g.score_home > 17.0 and g.score_home <=27.0:
-                pa = 0.0
-            if g.score_home > 27.0 and g.score_home <=34.0:
-                pa = -1.0
-            if g.score_home > 34.0 and g.score_home <=45.0:
-                pa = -3.0
-            if g.score_home >45.0:
-                pa = -5.0
-            #endif
-        return pa
-        #endif
-    #endfor  
-#endDef
-=======
         if p.defense_sk > 0:
             sack += 1
     return 2*dffum + 2*dint + 6*dinttd + 6*dfrectd + 2*dsafe + 6*punttd + 6*kicktd + 1*sack
@@ -181,4 +121,3 @@ def fpDefense(team, year, week):
     points = points + defensePA(team, year, week, games)
     points = points + defenseYA(team, year, week, games)
     return points
->>>>>>> origin/master
